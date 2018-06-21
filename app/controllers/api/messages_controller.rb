fcce48d025c72e260d88a77a80fd678adf0b26b7
yungwifi <smerryman@me.com>
2018-06-21 18:25:53 -0400

    halfway done with CRUD on messages

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,2 +1,34 @@
 class Api::MessagesController < ApplicationController
+    def index 
+        @message = Language.find(params[:language_id]).messages
+        render json: @message
+    end
+
+    def show
+        @message = Message.find(params[:id])
+        render json: @message
+    end
+
+    def create 
+        @message= Language.find(params[:language_id]).messages
+        @message.create!(message_params)
+        render json: @message
+    end
+
+    def update 
+        @message= Message.find(params[:id])
+        @message.update!(message_params)
+        render json: @message
+    end
+
+    def destroy 
+        @message = message.find(params[:id])
+        @message.destroy! 
+        render status: 200
+    end
+
+    private
+    def message_params
+        params.require(:message).permit(:message, :sender, :recipient)
+    end
 end
